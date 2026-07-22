4ab505b81cb2a9061572427bef605dd3df6360b7
kevin525 <kevin5251984@gmail.com>
2026-07-22 17:36:20 +0800

    fix(android): add missing Daqian keys ㄥ(/) ㄤ(;) ㄦ(-)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.xiapin.ime"
         minSdk = 21
         targetSdk = 34
-        versionCode = 22
-        versionName = "0.2.6"
+        versionCode = 23
+        versionName = "0.2.7"
 
         ndk {
             // 與 librime 靜態庫編譯的 ABI 對齊
